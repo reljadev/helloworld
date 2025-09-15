f35a810d1eba257fa543512b4406d0717e6556eb
Milan Reljin <milan.reljin@tttech.com>
2025-09-15 13:17:58 +0000

    removed 8443 port

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,7 +86,7 @@ resource "azurerm_network_security_group" "nsg" {
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_ranges    = ["443", "8443"] # TODO: remove 8443
+    destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
